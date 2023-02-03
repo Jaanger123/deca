3a8ff5ad74fbac684491e2924c2bd6df1dcef688
Jaanger123 <jbarakanov@inbox.ru>
2023-02-04 00:57:51 +0600

    editing teacher essay page

--- src/components/AuthMain/AuthMain.tsx
+++ src/components/AuthMain/AuthMain.tsx
@@ -1,24 +1,24 @@
+import { Link, useLocation } from 'react-router-dom';
 // import './AuthMain.scss';
 import 'components/AuthMain/AuthMain.scss';
-import { Link } from 'react-router-dom';
 
 const AuthMain = () => {
+    const location = useLocation();
+    const hasUser = location.pathname === '/sign-in';
+
     return (
         <div className="auth-main">
             <div className="auth">
                 <div className="auth-banner">
                     <img
-                        src={
-                            require('../../assets/images/auth-banner.svg')
-                                .default
-                        }
+                        src={require('assets/images/auth-banner.svg').default}
                         alt="Players banner"
                     />
                     <h4>Explore new adventures with the ones you love!</h4>
                     <p>Innovative board games now in Kyrgyzstan!</p>
                 </div>
                 <div className="auth-form-wrapper">
-                    <h2>Welcome!</h2>
+                    <h2>{hasUser ? 'Welcome back!' : 'Welcome!'}</h2>
                     <div className="auth-form">
                         <div className="auth-form-inputs">
                             <div className="form-input">
@@ -31,22 +31,28 @@ const AuthMain = () => {
                             </div>
                         </div>
                         <div className="form-buttons">
-                            <button className="auth-sign-in-up">Sign up</button>
+                            <button className="auth-sign-in-up">
+                                {hasUser ? 'Sign in' : 'Sign up'}
+                            </button>
                             <span>OR</span>
                             <button className="auth-sign-in-up-google">
                                 <img
                                     src={
-                                        require('../../assets/images/google-icon.svg')
+                                        require('assets/images/google-icon.svg')
                                             .default
                                     }
                                 />
-                                Sign up with Google
+                                {hasUser ? 'Sign in' : 'Sign up'} with Google
                             </button>
                         </div>
                     </div>
                     <span>
-                        Already have an account?{' '}
-                        <Link to="/sign-in">Sign in</Link>
+                        {hasUser
+                            ? 'Don’t have an account? '
+                            : 'Already have an account? '}
+                        <Link to={hasUser ? '/sign-up' : '/sign-in'}>
+                            {hasUser ? 'Sign up' : 'Sign in'}
+                        </Link>
                     </span>
                 </div>
             </div>
